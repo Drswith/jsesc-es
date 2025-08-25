38108fd191aaee33760f4d840b6c945d3c9b36f2
Drswith <drswith@outlook.com>
2025-08-26 02:25:02 +0800

    build: 更新版本号至0.0.1并改进tsdown配置
    将package.json中的版本号从0.0.0更新至0.0.1。同时重构tsdown配置，改为异步读取package.json文件以获取版本号，提高配置的灵活性。

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,19 +1,23 @@
+import fsp from 'node:fs/promises'
 import { defineConfig } from 'tsdown'
-import pkg from './package.json'
 
-export default defineConfig([
-  {
-    entry: ['./src/index.ts'],
-    platform: 'node',
-    format: ['esm', 'cjs'],
-    dts: true,
-    sourcemap: true,
-    target: ['node6', 'chrome27', 'firefox3', 'safari4', 'opera10'],
-    env: {
-      __VERSION__: pkg.version,
+export default defineConfig(async () => {
+  const pkg = JSON.parse(await fsp.readFile('./package.json', 'utf-8'))
+
+  return [
+    {
+      entry: ['./src/index.ts'],
+      platform: 'node',
+      format: ['esm', 'cjs'],
+      dts: true,
+      sourcemap: true,
+      target: ['node6', 'chrome27', 'firefox3', 'safari4', 'opera10'],
+      env: {
+        __VERSION__: pkg.version,
+      },
+      outputOptions: {
+        exports: 'named',
+      },
     },
-    outputOptions: {
-      exports: 'named',
-    },
-  },
-])
+  ]
+})
